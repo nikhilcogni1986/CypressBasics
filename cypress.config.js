44e61cab98681f860f195788c202478eac1d04c1
nikhilcogni <nikhilcogni@gmail.com>
2023-01-05 18:43:31 +0000

    Cucumber implementation

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
 module.exports = defineConfig({
   defaultCommandTimeout: 6000,
   env: {
@@ -6,6 +19,8 @@ module.exports = defineConfig({
   },
   e2e: {
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    excludeSpecPattern: ['**/1-getting-started/*','**/2-advanced-examples/*']
-  }
+    excludeSpecPattern: ['**/1-getting-started/*','**/2-advanced-examples/*'],
+    setupNodeEvents,
+  },
+
 });
