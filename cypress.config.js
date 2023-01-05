6ddae410c0dac1b26d5df84b58e20ddcc0e5d7c8
nikhilcogni <nikhilcogni@gmail.com>
2023-01-05 17:43:13 +0000

    End to End test Test8Framework

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
+  defaultCommandTimeout: 6000,
+  env: {
+    url: "https://rahulshettyacademy.com",
+  },
   e2e: {
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     excludeSpecPattern: ['**/1-getting-started/*','**/2-advanced-examples/*']
--- cypress/e2e/Test8Framework.js
+++ cypress/e2e/Test8Framework.js
@@ -14,7 +14,7 @@ describe('My Second Test Suite', function () {
   it('My FirstTest case', function () {
     const homePage = new HomePage()
     const productPage = new ProductPage()
-    cy.visit("https://rahulshettyacademy.com/angularpractice/")
+    cy.visit(Cypress.env('url')+"/angularpractice/")
     homePage.getEditBox().type(this.data.name)
     homePage.getGender().select(this.data.gender)
     homePage.getTwoWayDataBinding().should('have.value', this.data.name)
