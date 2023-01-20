c95e0cd13bd7779b8e439e662464dc06a5abc230
nikhilcogni <nikhilcogni@gmail.com>
2023-01-20 16:50:33 +0000

    Minor changes

--- cypress/e2e/alerts.spec.js
+++ cypress/e2e/alerts.spec.js
@@ -7,7 +7,6 @@ describe('Alert Test Demo', function(){
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 
         //click on alert
-        
         cy.get('#name').type("Pranav")
         cy.get('#alertbtn').click()
 
@@ -21,6 +20,18 @@ describe('Alert Test Demo', function(){
         {
             //Mocha
             expect(str).to.equal('Hello Pranav, Are you sure you want to confirm?')
+        })
+    })
+
+    it.only('Alert test on another practice website', ()=>{
+        cy.visit("https://testautomationpractice.blogspot.com/")
+
+        //click on Alert button
+        cy.get('#HTML9 > .widget-content > button').click()
+        cy.on('window:confirm',(alert_text)=>
+        {
+            //Mocha
+            expect(alert_text).to.equal('Press a button!')
         })
     })
 })
